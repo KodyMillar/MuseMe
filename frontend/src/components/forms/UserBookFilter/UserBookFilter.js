1b936d2c496335f48868e4a1d924e0c3037c959c
Kody <kodymillar@hotmail.com>
2025-01-07 18:06:06 -0800

    Fix song piano filter buttons and finish design for right side piano filter button

--- frontend/src/components/forms/UserBookFilter/UserBookFilter.js
+++ frontend/src/components/forms/UserBookFilter/UserBookFilter.js
@@ -4,17 +4,17 @@ import styles from './UserBookFilter.module.css';
 function UserBookFilter(props) {
 
     return (
-        <form action={`/play/search/song-progress/<%= ${props.userId} %>`}
+        <form action={`/play/search/song-progress/${props.userId}`}
             method="get" 
             className={styles.userBookFilterButton}
-            id={styles[`#${props.formId}`]} 
+            id={styles[props.formId]} 
         >
-            <span class={styles.buttonEdge} id={styles["`#${props.spanId}`"]}></span>
+            <span className={styles.buttonEdge} id={styles[props.spanId]}></span>
             <input type="submit" 
                 value={props.value}
                 name="user-book-filter" 
                 className={styles.innerButton}
-                id={styles[`${props.inputId}`]}
+                id={styles[props.inputId]}
             />
         </form>
     );
